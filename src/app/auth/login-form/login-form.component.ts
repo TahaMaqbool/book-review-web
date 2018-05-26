9f3b397a7880edb6eb131f5fbc8ad4978b8848b3
taha <taha.maqbool96@gmail.com>
2018-05-26 20:44:44 +0500

    added model driven validataion

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Angular2TokenService} from 'angular2-token';
 import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -9,22 +10,42 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginFormComponent implements OnInit {
 
-  signInUser = {
-    email: '',
-    password: ''
-  };
+  loginForm: FormGroup;
 
   @Output() onFormResult = new EventEmitter<any>();
 
-  constructor(public authService: AuthService, public authTokenService: Angular2TokenService) {
+  constructor(public authService: AuthService,
+              public authTokenService: Angular2TokenService,
+              private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(8)
+      ]))
+    });
   }
+
+  account_validation_messages = {
+    'email': [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Enter a valid email' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required' },
+      { type: 'minlength', message: 'Password must be at least 8 characters long' },
+    ]
+  };
 
   ngOnInit() {
   }
 
 
   onSignInSubmit() {
-    this.authService.logInUser(this.signInUser).subscribe(
+    this.authService.logInUser(this.loginForm.value).subscribe(
       res => {
         if (res.status === 200) {
           this.onFormResult.emit({signedIn: true, res});
