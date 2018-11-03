1bfc215e5036a8af826fe9e3191e3cd4b952be1e
taha <taha.maqbool96@gmail.com>
2018-11-03 15:57:56 +0500

    added reset callback url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   token_auth_config: {
-    apiBase: 'https://book-review-api.herokuapp.com/api/v1'
+    apiBase: 'https://book-review-api.herokuapp.com/api/v1',
+    resetPasswordCallback: 'https://book-review-web.firebaseapp.com/reset'
   }
 };
